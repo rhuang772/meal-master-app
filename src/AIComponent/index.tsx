d8b309b8e8c32fb5d873409d879b7457d1a05478
Alex Miale <alexmiale2014@gmail.com>
2024-04-06 16:51:32 -0400

    AI is working, not fitting in textbox

--- src/AIComponent/index.tsx
+++ src/AIComponent/index.tsx
@@ -17,15 +17,15 @@ export class AI {
       this.goals = "";
   }
 
-  setModel(newModel:any) {
+  public setModel(newModel:any) {
       this.model = newModel;
   }
 
-  setInput(newInput:string) {
+  public setInput(newInput:string) {
       this.input = newInput;
   }
 
-  setParams(cookingScale: string, timesPerWeek: string, dietaryRestrictions: string, dietType: string, goals: string) {
+  public setParams(cookingScale: string, timesPerWeek: string, dietaryRestrictions: string, dietType: string, goals: string) {
       this.cookingScale = cookingScale;
       this.timesPerWeek = timesPerWeek;
       this.dietaryRestrictions = dietaryRestrictions;
@@ -33,18 +33,27 @@ export class AI {
       this.goals = goals;
   }
 
-  async run(): Promise<string> {
-    const prompt = "Generate some detailed recipes from the following information: The user rates their cooking a " + this.cookingScale + " on a scale from 1 to 5, 1 - instant ramen, 2 - can use a stove, 3 - comfortable with cooking, 4 - daily compliments on your cooking, and 5 being an expert. " +
+  public async run() {
+    const prompt = "Generate some detailed recipes from the following information: The user rates their cooking a " + this.cookingScale + 
+    " on a scale from 1 to 5, 1 - instant ramen, 2 - can use a stove, 3 - comfortable with cooking, 4 - daily compliments on your cooking, and 5 being an expert. " +
     "The user cooks " + this.timesPerWeek + " times per week. " + 
     "The user has the following dietary restrictions/allergies: " + this.dietaryRestrictions + 
     " The user adheres to the following diet: " + this.dietType + 
     " The user has the following goals when making a meal: " + this.goals + 
     this.input;
   
-    const result = await this.model.generateContent(prompt);
-    const response = await result.response;
-    const text = response.text().replace(/\*/g, "");
-    return text; // test output in console for now
+    try{
+      const result = await this.model.generateContent(prompt);
+      const response = await result.response;
+      const text = response.text().replace(/\*/g, "");
+      return text; // test output in console for now
+    } catch (e) {
+      if (typeof e === "string") {
+        return e.toUpperCase() // works, `e` narrowed to string
+      } else if (e instanceof Error) {
+        return e.message // works, `e` narrowed to Error
+      }
+    }
   }
 }
 
