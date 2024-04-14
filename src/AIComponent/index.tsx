aefa5257aaca5451372ea2a0a30bf57baede6f36
Alex Miale <alexmiale2014@gmail.com>
2024-04-14 13:59:28 -0400

    Reapply "two buttons into one, enter key no longer refreshes page"
    This reverts commit 9a9133050539333d311e5a1dce1fb8e689ab7426.

--- src/AIComponent/index.tsx
+++ src/AIComponent/index.tsx
@@ -19,6 +19,7 @@ export class AI {
       this.style = "";
   }
 
+  // public setters
   public setModel(newModel:any) {
       this.model = newModel;
   }
@@ -27,17 +28,33 @@ export class AI {
       this.input = newInput;
   }
 
-  public setParams(cookingScale: string, timesPerWeek: string, dietaryRestrictions: string, dietType: string, goals: string, style: string) {
-      this.cookingScale = cookingScale;
-      this.timesPerWeek = timesPerWeek;
-      this.dietaryRestrictions = dietaryRestrictions;
-      this.dietType = dietType;
-      this.goals = goals;
-      this.style = style;
+  public setScale(newScale:string) {
+    this.cookingScale = newScale;
   }
 
+  public setTime(newTime:string) {
+    this.timesPerWeek = newTime;
+  }
+
+  public setDietRestrictions(newDiet:string) {
+    this.dietaryRestrictions = newDiet;
+  }
+
+  public setDietType(newType:string) {
+    this.dietType = newType;
+  }
+
+  public setGoals(newGoals:string) {
+    this.goals = newGoals;
+  }
+
+  public setStyle(newStyle:string) {
+    this.style = newStyle;
+  }
+  
+  // call to the model for a response
   public async run() {
-    const prompt = "You are a recipe generator. You do not need to return or print out any of the given information. " +
+    const prompt = "You are a recipe generator. You do not need to return or print out any of the given information. All responses must be in English. " +
     "Generate some detailed recipes from the following information: The user rates their cooking a " + this.cookingScale + 
     " on a scale from 1 to 5, 1 - instant ramen, 2 - can use a stove, 3 - comfortable with cooking, 4 - daily compliments on your cooking, and 5 being an expert. " +
     "The user cooks " + this.timesPerWeek + " times per week. " + 
@@ -51,7 +68,7 @@ export class AI {
       const result = await this.model.generateContent(prompt);
       const response = await result.response;
       const text = response.text().replace(/\*/g, "");
-      return text; // test output in console for now
+      return text; 
     } catch (e) {
       if (typeof e === "string") {
         return e.toUpperCase() // works, `e` narrowed to string
